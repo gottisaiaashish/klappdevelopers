8f4a1aa8727b089442cb93aef81699bc0bedae3f
gottisaiaashish <gottisaiaashish@gmail.com>
2026-07-31 16:16:49 +0530

    feat: connect MongoDB Atlas database cluster for permanent lead storage

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,6 @@
+const dns = require('dns');
+try { dns.setServers(['8.8.8.8', '8.8.4.4']); } catch (e) {}
+
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
